e3fd0e6a590557ae6bb727218498fd08a5c5ed5c
AgungPremaditya <premadityamandala@gmail.com>
2021-12-22 11:24:34 +0800

    add loading

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -1,17 +1,15 @@
-<script setup lang="ts"></script>
-
 <template>
   <link rel="stylesheet" href="./style/Login.css" />
   <div class="center">
     <h1>Login</h1>
-    <form method="post">
+    <form @submit.prevent="submit">
       <div class="txt_field">
-        <input type="text" required />
+        <input type="text" v-model="payload.username" required />
         <span></span>
         <label>Username</label>
       </div>
       <div class="txt_field" style="margin-bottom: 3em">
-        <input type="password" required />
+        <input type="password" v-model="payload.password" required />
         <span></span>
         <label>Password</label>
       </div>
@@ -20,3 +18,28 @@
     </form>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  setup() {
+    const router = useRouter();
+    const payload = ref({
+      username: "",
+      password: "",
+    });
+
+    const submit = () => {
+      localStorage.setItem("auth-key", JSON.stringify(payload.value));
+      router.replace({ name: "home" });
+    };
+
+    return {
+      payload,
+      submit,
+    };
+  },
+});
+</script>
