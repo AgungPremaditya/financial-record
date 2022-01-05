4ab07819c52a812232e078d7e722c2387b1ccb94
AgungPremaditya <premadityamandala@gmail.com>
2022-01-05 22:52:41 +0800

    try to deploy

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -1,44 +1,3 @@
-<template>
-  <div>
-    <div class="background" v-if="loading">
-      <Loading></Loading>
-    </div>
-    <div class="center" :style="loading ? 'z-index: -99' : 'z-index: 0'">
-      <h1>Sign In</h1>
-      <form @submit.prevent="submit" :validation-schema="loginFormSchema">
-        <div class="txt_field">
-          <input type="text" v-model="payload.email" required />
-          <span></span>
-          <label>Email</label>
-        </div>
-        <div class="txt_field" style="margin-bottom: 1em">
-          <input type="password" v-model="payload.password" required />
-          <span></span>
-          <label>Password</label>
-        </div>
-        <div class="error_text" style="margin-bottom: 2em" v-if="errors">
-          {{ errors }}
-        </div>
-        <button type="submit" class="btnInput" :disabled="loading">
-          Login
-        </button>
-        <div class="signup_link">
-          Not a member? <router-link to="/register">Signup</router-link>
-        </div>
-      </form>
-    </div>
-  </div>
-</template>
-
-<style>
-.background {
-  background-color: rgba(0, 0, 0, 0.4);
-  width: 100%;
-  height: 100vh;
-  z-index: 99;
-}
-</style>
-
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { useRouter } from "vue-router";
@@ -93,3 +52,44 @@ export default defineComponent({
   components: { Loading },
 });
 </script>
+
+<template>
+  <div>
+    <div class="background" v-if="loading">
+      <Loading></Loading>
+    </div>
+    <div class="center" :style="loading ? 'z-index: -99' : 'z-index: 0'">
+      <h1>Sign In</h1>
+      <form @submit.prevent="submit" :validation-schema="loginFormSchema">
+        <div class="txt_field">
+          <input type="text" v-model="payload.email" required />
+          <span></span>
+          <label>Email</label>
+        </div>
+        <div class="txt_field" style="margin-bottom: 1em">
+          <input type="password" v-model="payload.password" required />
+          <span></span>
+          <label>Password</label>
+        </div>
+        <div class="error_text" style="margin-bottom: 2em" v-if="errors">
+          {{ errors }}
+        </div>
+        <button type="submit" class="btnInput" :disabled="loading">
+          Login
+        </button>
+        <div class="signup_link">
+          Not a member? <router-link to="/register">Signup</router-link>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style>
+.background {
+  background-color: rgba(0, 0, 0, 0.4);
+  width: 100%;
+  height: 100vh;
+  z-index: 99;
+}
+</style>
